6c945b1dae85813701a2ea82d2eb1a573a345d8e
Tumlinh <tumlinh@protonmail.com>
2018-10-14 15:50:16 +0200

    Spawn bodyguards naturally Change parent class to EntityCreature

--- src/main/java/mineguard/entity/EntityBodyguard.java
+++ src/main/java/mineguard/entity/EntityBodyguard.java
@@ -1,19 +1,22 @@
 package mineguard.entity;
 
 import java.util.IllegalFormatException;
+import javax.annotation.Nullable;
 import mineguard.Troop;
 import mineguard.entity.ai.EntityAIBehaviour;
 import mineguard.entity.ai.EntityAIReform;
 import mineguard.init.ModConfig;
 import mineguard.util.EntityUtil;
-import net.minecraft.block.Block;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -21,11 +24,11 @@ import net.minecraft.nbt.NBTTagInt;
 import net.minecraft.nbt.NBTTagString;
 import net.minecraft.pathfinding.PathNavigateGround;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
-public class EntityBodyguard extends EntityMob
+public class EntityBodyguard extends EntityCreature
 {
     private int id;
     private Troop troop;
@@ -39,6 +42,7 @@ public class EntityBodyguard extends EntityMob
         // #2. Add bodyguard to player's troop (call other constructor)
         super(worldIn);
         this.setSize(0.6F, 1.8F);
+        this.enablePersistence();
     }
 
     // Called when spawning new bodyguards as part of a troop
@@ -49,8 +53,6 @@ public class EntityBodyguard extends EntityMob
         this.troop = troop;
         if (troop != null)
             troop.addBodyguard(this);
-
-        this.enablePersistence();
 
         // Name bodyguard
         this.updateName();
@@ -68,11 +70,6 @@ public class EntityBodyguard extends EntityMob
     public int getId()
     {
         return id;
-    }
-
-    public void setId(int id)
-    {
-        this.id = id;
     }
 
     public Troop getTroop()
@@ -132,6 +129,21 @@ public class EntityBodyguard extends EntityMob
         EntityUtil.setEntityAttribute(this, SharedMonsterAttributes.FOLLOW_RANGE, ModConfig.BODYGUARD_FOLLOW_RANGE);
     }
 
+    // XXX: debugging
+    @Override
+    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata)
+    {
+        livingdata = super.onInitialSpawn(difficulty, livingdata);
+        System.out.println("<<<spawn: " + this);
+        return livingdata;
+    }
+
+    @Override
+    public boolean getCanSpawnHere()
+    {
+        return super.getCanSpawnHere() && this.getEntityBoundingBox().minY >= 80;
+    }
+
     @Override
     public void onDeath(DamageSource cause)
     {
@@ -157,24 +169,6 @@ public class EntityBodyguard extends EntityMob
     }
 
     @Override
-    protected SoundEvent getHurtSound(DamageSource damageSourceIn)
-    {
-        return SoundEvents.ENTITY_HOSTILE_HURT;
-    }
-
-    @Override
-    protected SoundEvent getDeathSound()
-    {
-        return SoundEvents.ENTITY_HOSTILE_DEATH;
-    }
-
-    @Override
-    protected void playStepSound(BlockPos pos, Block blockIn)
-    {
-        super.playStepSound(pos, blockIn);
-    }
-
-    @Override
     protected float getWaterSlowDown()
     {
         return 0.85F;
@@ -188,9 +182,8 @@ public class EntityBodyguard extends EntityMob
             id = compound.getInteger("Id");
         }
         if (compound.hasKey("Master", new NBTTagString().getId())) {
-            troop = Troop.getTroop(compound.getString("Master"));
-
             // Add bodyguard to troop
+            troop = Troop.getTroop(compound.getString("Master"));
             troop.addBodyguard(this);
         }
     }
@@ -203,6 +196,53 @@ public class EntityBodyguard extends EntityMob
             compound.setInteger("Id", id);
             compound.setString("Master", troop.getMasterName());
         }
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn)
+    {
+        float attackDamage = (float) this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+        int knockbackModifier = 0;
+        if (entityIn instanceof EntityLivingBase) {
+            attackDamage += EnchantmentHelper.getModifierForCreature(this.getHeldItemMainhand(),
+                    ((EntityLivingBase) entityIn).getCreatureAttribute());
+            knockbackModifier += EnchantmentHelper.getKnockbackModifier(this);
+        }
+
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), attackDamage);
+        if (flag) {
+            if (knockbackModifier > 0 && entityIn instanceof EntityLivingBase) {
+                ((EntityLivingBase) entityIn).knockBack(this, (float) knockbackModifier * 0.5F,
+                        (double) MathHelper.sin(this.rotationYaw * 0.017453292F),
+                        (double) (-MathHelper.cos(this.rotationYaw * 0.017453292F)));
+                this.motionX *= 0.6D;
+                this.motionZ *= 0.6D;
+            }
+
+            int fireModifier = EnchantmentHelper.getFireAspectModifier(this);
+            if (fireModifier > 0)
+                entityIn.setFire(fireModifier * 4);
+
+            if (entityIn instanceof EntityPlayer) {
+                EntityPlayer entityplayer = (EntityPlayer) entityIn;
+                ItemStack mainHandItem = this.getHeldItemMainhand();
+                ItemStack activeItem = entityplayer.isHandActive() ? entityplayer.getActiveItemStack()
+                        : ItemStack.EMPTY;
+                if (!mainHandItem.isEmpty() && !activeItem.isEmpty()
+                        && mainHandItem.getItem().canDisableShield(mainHandItem, activeItem, entityplayer, this)
+                        && activeItem.getItem().isShield(activeItem, entityplayer)) {
+                    float f1 = 0.25F + (float) EnchantmentHelper.getEfficiencyModifier(this) * 0.05F;
+                    if (this.rand.nextFloat() < f1) {
+                        entityplayer.getCooldownTracker().setCooldown(activeItem.getItem(), 100);
+                        this.world.setEntityState(entityplayer, (byte) 30);
+                    }
+                }
+            }
+
+            this.applyEnchantments(this, entityIn);
+        }
+
+        return flag;
     }
 
     @Override
